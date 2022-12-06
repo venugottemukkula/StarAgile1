8b29149e4b57945193538b3fd01114960e379b6f
venu <venugottemukkula@gmail.com>
2022-12-06 23:46:34 +0530

    Project created

---
+++ Project/src/test/java/com/staragile/ProjectImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staragile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
